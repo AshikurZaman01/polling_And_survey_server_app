ad19791eb455a8b0aaffb753fd9c157c99f1c3f0
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-11-28 23:22:47 +0600

    connect with mongodb

--- index.js
+++ index.js
@@ -1,17 +1,42 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
-var jwt = require('jsonwebtoken');
+const app = express();
 const port = process.env.PORT || 3000;
 
-// middleware
 app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Poll server is running')
+    res.send('Survey POrt is Running');
 })
 
 app.listen(port, () => {
-    console.log(`Poll running  on port ${port}`);
+    console.log(`Survey is running on port ${port}`);
 })
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://pollAndSurvey:WA76XmqhU1rOsHrD@cluster0.q0gttvx.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
