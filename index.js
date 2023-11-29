559c3785a1233ce178147c15985f3badcfbc94ec
AshikurZaman01 <ashikurzaman774@gmail.com>
2023-11-29 16:45:18 +0600

    make admin api

--- index.js
+++ index.js
@@ -60,6 +60,18 @@ async function run() {
             res.send(result);
             console.log(result);
         })
+        app.patch('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin',
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc);
+            res.send(result);
+            console.log(result);
+        })
         // users end
 
 
